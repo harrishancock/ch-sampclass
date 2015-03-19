ccc45073af677de36ad97bb753ab1f7fcbdceb71
Harris Hancock <vortrab@gmail.com>
2015-03-19 12:49:23 -0700

    Uncomment crashing line

--- script.cpp
+++ script.cpp
@@ -2,7 +2,7 @@
 int main() {
 	class Class1 *pc2;
 
-	//class Class1 ac[NUM]; // This line crashes in Windows
+	class Class1 ac[NUM]; // This line crashes in Windows
 
 	pc2 = new Class1[2]; // This line crashes in Windows
 	pc2[0].memfun1(10);
